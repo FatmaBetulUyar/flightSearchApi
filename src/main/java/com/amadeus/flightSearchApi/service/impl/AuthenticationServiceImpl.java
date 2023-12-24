ed194a4c42da177d6fec45eba44b9304143d6619
Betül <69481739+FatmaBetulUyar@users.noreply.github.com>
2023-12-24 12:00:21 +0300

    added generic exception handling

--- src/main/java/com/amadeus/flightSearchApi/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/amadeus/flightSearchApi/service/impl/AuthenticationServiceImpl.java
@@ -5,10 +5,12 @@ import com.amadeus.flightSearchApi.dto.SignUpRequest;
 import com.amadeus.flightSearchApi.dto.SigninRequest;
 import com.amadeus.flightSearchApi.entity.Role;
 import com.amadeus.flightSearchApi.entity.User;
+import com.amadeus.flightSearchApi.exception.CustomJwtException;
 import com.amadeus.flightSearchApi.repository.UserRepository;
 import com.amadeus.flightSearchApi.service.AuthenticationService;
 import com.amadeus.flightSearchApi.service.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,20 +25,24 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final AuthenticationManager authenticationManager;
     @Override
     public JwtAuthenticationResponse signup(SignUpRequest request) {
-        var user = User.builder().firstName(request.getFirstName()).lastName(request.getLastName())
-                .email(request.getEmail()).password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER).build();
-        userRepository.save(user);
-        var jwt = jwtService.generateToken(user);
-        return JwtAuthenticationResponse.builder().token(jwt).build();
-    }
+        if (!userRepository.existsByEmail(request.getEmail())){
+            var user = User.builder().firstName(request.getFirstName()).lastName(request.getLastName())
+                    .email(request.getEmail()).password(passwordEncoder.encode(request.getPassword()))
+                    .role(Role.USER).build();
+            userRepository.save(user);
+            var jwt = jwtService.generateToken(user);
+            return JwtAuthenticationResponse.builder().token(jwt).build();
+        }else {
+            throw new CustomJwtException("Email is already in use", HttpStatus.BAD_REQUEST);
+        }
 
+    }
     @Override
     public JwtAuthenticationResponse signin(SigninRequest request) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
         var user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
+                .orElseThrow(() -> new  CustomJwtException("Invalid username/password supplied", HttpStatus.BAD_REQUEST));
         var jwt = jwtService.generateToken(user);
         return JwtAuthenticationResponse.builder().token(jwt).build();
     }
